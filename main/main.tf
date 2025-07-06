78583d1e9e6a71da57167a6831a615eecd3aa7d5
Steve Sedor <ssedor1@outlook.com>
2025-07-06 09:51:02 -0400

    test

--- main/main.tf
+++ main/main.tf
@@ -29,25 +29,25 @@ resource "azurerm_network_security_group" "example" {
   resource_group_name = azurerm_resource_group.app_service.name
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  location            = azurerm_resource_group.app_service.location
-  resource_group_name = azurerm_resource_group.app_service.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name             = "subnet1"
-    address_prefixes = ["10.0.1.0/24"]
-  }
-
-  subnet {
-    name             = "subnet2"
-    address_prefixes = ["10.0.2.0/24"]
-    security_group   = azurerm_network_security_group.example.id
-  }
-
-  tags = {
-    environment = "Production"
-  }
-}
+#resource "azurerm_virtual_network" "example" {
+#  name                = "example-network"
+#  location            = azurerm_resource_group.app_service.location
+#  resource_group_name = azurerm_resource_group.app_service.name
+#  address_space       = ["10.0.0.0/16"]
+#  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+#
+#  subnet {
+#    name             = "subnet1"
+#    address_prefixes = ["10.0.1.0/24"]
+#  }
+#
+#  subnet {
+#    name             = "subnet2"
+#    address_prefixes = ["10.0.2.0/24"]
+#    security_group   = azurerm_network_security_group.example.id
+#  }
+#
+#  tags = {
+#    environment = "Production"
+#  }
+#}

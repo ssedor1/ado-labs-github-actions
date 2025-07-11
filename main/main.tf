9a63d8a0de9460b7c6c133153b156ec74bd1403d
Steve Sedor <ssedor1@outlook.com>
2025-07-11 12:40:01 -0400

    test

--- main/main.tf
+++ main/main.tf
@@ -23,11 +23,11 @@ resource "azurerm_resource_group" "app_service" {
   location = var.location
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.app_service.location
-  resource_group_name = azurerm_resource_group.app_service.name
-}
+#resource "azurerm_network_security_group" "example" {
+#  name                = "example-security-group"
+#  location            = azurerm_resource_group.app_service.location
+#  resource_group_name = azurerm_resource_group.app_service.name
+#}
 
 #resource "azurerm_virtual_network" "example" {
 #  name                = "example-network"
@@ -51,3 +51,35 @@ resource "azurerm_network_security_group" "example" {
 #    environment = "Production"
 #  }
 #}
+
+
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "acctest-01"
+  location            = azurerm_resource_group.app_service.location
+  resource_group_name = azurerm_resource_group.app_service.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "example" {
+  name                       = "Example-Environment"
+  location            = azurerm_resource_group.app_service.location
+  resource_group_name = azurerm_resource_group.app_service.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+}
+
+resource "azurerm_container_app" "example" {
+  name                         = "example-app"
+  container_app_environment_id = azurerm_container_app_environment.example.id
+  resource_group_name = azurerm_resource_group.app_service.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "examplecontainerapp"
+      image  = "mcr.microsoft.com/k8se/quickstart:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+}
